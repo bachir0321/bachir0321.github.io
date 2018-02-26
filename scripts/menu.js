41a7092eea4d593ba91d917370f8edbb83fc500a
Brian Bachir <bachir0321@gmail.com>
2018-02-26 18:47:49 -0500

    1.10
    Hamburger upgrayyyyyd

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
     $(".burger-nav img").on("click",function(){
-
         $("header nav ul").toggleClass("open");
+    });
 
+    $(".header-right").on("click",function(){
+        $("header nav ul").toggleClass("open");
     });
 
 });
-
-// bug
